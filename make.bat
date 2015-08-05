c02f85678b782d20c8df475d33dd1069e216f6a8
lute sasaki <gyaboyan@gmail.com>
2015-08-05 22:32:12 +0900

    aa

--- make.bat
+++ make.bat
@@ -1,21 +1,4 @@
 @echo off
 ctags -R
 
-<<<<<<< HEAD
 cl  main.cpp util.cpp /MP4 /Ox /EHsc /GS- /nologo /Zi /DEBUG:FASTLINK
-=======
-REM cl  main.cpp util.cpp /MP4 /Ox /EHsc /GS- /nologo  /c
-cl  main.cpp util.cpp /MP4 /Ox /EHsc /GS- /nologo /Z7 /c /DEBUG:FASTLINK
-
-link *.obj /nologo
-REM del *.obj
-@echo on
-
-REM start main.exe
-REM 
-REM vim .
-REM 
-REM 
-REM kill
->>>>>>> 3513a1b78996d0048adf12b477ae4cfc8753b450
-
